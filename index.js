b5a8eef01f517d3f5ce3c450fe5b799a7b5b0688
Lcf.vs <lcf.vs@hotmail.com>
2014-04-04 11:12:06 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+#!/home/nodejs/local/bin/node
+
+/*
+### BEGIN INIT INFO
+# Provides:          node-server
+# Required-Start:    $local_fs $network $syslog
+# Required-Stop:     $local_fs $network $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Start daemon at boot time
+# Description:       Enable service provided by node-server.
+### END INIT INFO
+*/
+
+console.log('Node.js interprets this service script');
